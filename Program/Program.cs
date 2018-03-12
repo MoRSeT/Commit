96c3762ec248a0c487d736cb384cb97797a919b9
Makarov Vadim <sukab228@outlook.com>
2018-03-12 20:43:36 +0300

    3-й commit

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
-namespace Program
+
+namespace Collections
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Введите количество студентов: ");
+            int size_list = Convert.ToInt32(Console.ReadLine());
+            // с помощью конструктора с параметрами создаем объект класса car_list 
+            // (сразу устанавливаем емкость списка внутри класса) 
+            ProductList car_list = new ProductList(size_list);
+            // заполнение данными 
+            for (int i = 0; i < size_list; i++)
+            {
+                car_list.AddItem();
+            }
+            Console.Clear();
+            // используем индексатор 
+            // car_list[i] - возвращает целый объект класса Car 
+            // используем метод Print(), который мы реализовали в классе Car 
+            // для вывода значений объекта класса 
+            for (int i = 0; i < size_list; i++)
+            {
+                car_list[i].Print();
+            }
+            Console.ReadKey();
+
         }
     }
 }
